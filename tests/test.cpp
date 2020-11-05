ceb8c0f5b24b4ed86c4720e42f0f813a00d9c4f9
slagu002 <slagu002@ucr.edu>
2020-11-05 11:52:54 -0800

    updated test file for area and perimeter

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,45 @@
 #include "gtest/gtest.h"
 #include "../header/rectangle.hpp"
 
-TEST(RectTest, Area) {
+//Area
+TEST(RectTest, AreaZeroCheck) {
     Rectangle *rect = new Rectangle();
     EXPECT_EQ(rect->area(), 0);
+}
+
+TEST(RectTest, AreaOneCheck) {
+    Rectangle *rect = new Rectangle();
+    rect->set_width(1);
+    rect->set_height(1);
+    EXPECT_EQ(rect->area(),1);
+}
+
+TEST(RectTest, AreaPosCheck) {
+   Rectangle *rect = new Rectangle();
+   rect->set_width(5);
+   rect->set_height(5);
+   EXPECT_EQ(rect->area(), 25);
+}
+
+
+//Perimeter
+TEST(RectTest, PerimeterZeroCheck) {
+    Rectangle *rect = new Rectangle();
+    EXPECT_EQ(rect->perimeter(), 0);
+}
+
+TEST(RectTest, PerimeterPosCheck) {
+    Rectangle *rect = new Rectangle();
+    rect->set_width(5);
+    rect->set_height(5);
+    EXPECT_EQ(rect->perimeter(), 20);
+}
+
+TEST(RectTest, PerimeterOneCheck) {
+    Rectangle *rect = new Rectangle();
+    rect->set_width(1);
+    rect->set_height(1);
+    EXPECT_EQ(rect->perimeter(), 4);
 }
 
 int main(int argc, char **argv) {
